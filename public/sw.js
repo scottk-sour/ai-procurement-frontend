ca56207a26de18a2c0ce3a8211340b1ec56f925d
scottk-sour <122282065+scottk-sour@users.noreply.github.com>
2025-09-12 14:14:02 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+// public/sw.js
+const CACHE_NAME = 'tendor-ai-cache-v1';
+const urlsToCache = ['/', '/index.html', '/static/js/main.js'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching assets');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
